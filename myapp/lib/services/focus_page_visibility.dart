b96c59ff1e42650810dc48041f6c41a00471e101
crlx1q <104364472+crlx1q@users.noreply.github.com>
2026-02-18 18:16:02 +0500

    Delete myapp directory

--- myapp/lib/services/focus_page_visibility.dart
+++
@@ -1,9 +0,0 @@
-class FocusPageVisibility {
-  static bool _isOnFocusPage = false;
-
-  static bool get isOnFocusPage => _isOnFocusPage;
-
-  static void setOnFocusPage(bool value) {
-    _isOnFocusPage = value;
-  }
-}
